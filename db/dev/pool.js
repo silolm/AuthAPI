5a2b248f88c80ebb136158da7e174a85ac25f4a6
Mario Silla Olmos <silolm.mail@gmail.com>
2020-03-19 21:20:25 +0100

    creación de estructura y base de datos

--- db/dev/pool.js
+++
@@ -1,9 +0,0 @@
-import {Pool} from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const databaseConfig = {connectionString: "postgres://transportUser:develop@192.168.0.146:5235/transport"};
-const pool = new Pool(databaseConfig);
-
-export default pool;
