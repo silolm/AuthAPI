0e309fbabe2d9bdd4ed42dfc57d7d1d3dd7b8b3b
Mario Silla Olmos <silolm.mail@gmail.com>
2020-05-02 17:41:10 +0200

    test cors

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    config = require('./config/config');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  config = require("./config/config");
 app = express();// 1
 
-app.set('masterKey', config.masterKey);// 2
-app.use(bodyParser.urlencoded({extended: true}));// 3
+app.set("masterKey", config.masterKey);// 2
+app.use(bodyParser.urlencoded({ extended: true }));// 3
 app.use(bodyParser.json());// 4
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+});
 
-require('./app/routes/authAPI.routes')(app);
+require("./app/routes/authAPI.routes")(app);
 
 app.listen(3000, () => {
-    console.log('Corriendo en http://localhost:3000')
+  console.log("Corriendo en http://localhost:3000");
 });// 5
