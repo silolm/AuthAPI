5a065d8775341d94ed7807b073ad0935dfd74eca
Mario Silla Olmos <silolm.mail@gmail.com>
2020-05-02 17:45:55 +0200

    more settings of CORS, all its OK

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ app.use(bodyParser.urlencoded({ extended: true }));// 3
 app.use(bodyParser.json());// 4
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY," +
+    " Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 require("./app/routes/authAPI.routes")(app);
